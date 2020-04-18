16e86008c771d2699728f9ab1138f7609618f612
Myroslav Voysovych <mirovois@gmail.com>
2020-04-18 01:19:20 -0400

    The whole html file was completely renowated. Js file was partially modified.

--- script.js
+++ script.js
@@ -1,57 +1,108 @@
+var credit = 300;
+
+var inputController = (function() {
+
+})
+
 var transferController = (function() {
-
-   var credit = 300;
-   var chk = [];
-
-   var a=3;
-   return {
-     test:function(b){
-
-        console.log(a+b);
-   }
- }
-})();
-
-var UIContoller = (function(){
-  return {
-    getInput:function() {
-      var amountAdded = document.querySelector('.add_value').value;
-      return amountAdded;
-    },
-    addToChecking: function() {
-      var amountTransferedChecking = document.querySelector('.item-value');
-    }
+ 
+  var Transfer = function(sender,destination,value,) {
+    this.sender = sender;
+    this.value = value;
+    this.destination = destination;
   }
 
+  var transfers = {
+    allTransfers: {
+      chk:[],
+      crd:[]
+  },
+    total: {
+      chk:500,
+      crd:300
+    }
+  };
+  // var chkValue;
+
+  return {
+      addTransfer: function(accFrom,accTo,val) {
+      var newTransfer;
+      
+      // Create new item based on 'chk' or 'crd' type
+      if (acc==='chk') {
+        newTransfer = new Transfer(val);
+      } else if (acc==='crd')
+        newTransfer = new Transfer(val)
+      //Push it into data structure
+        transfers.allTransfers[acc].push(newTransfer);
+      return newTransfer;
+    },
+      testing:function() {
+        console.log(transfers);
+      }
+  };
 })();
 
-var controller = (function(transdCTRL,UICtrl){
 
+var UIContoller = (function(){
+    
+  var DOMstrings= {
+      inputValue:'.add_value',
+      inputValue:'.transfer_value',
+      senderAccount: '.account_sender',
+      destinatioAccount: '.account_destination',
+      inputButton:'.add_btn'
+    };
+    
+  return {
+    getInput:function() {
+      return {
+        
+        amount:parseFloat(document.querySelector(DOMstrings.inputValue).value),
+        accountFrom: document.querySelector(DOMstrings.senderAccount).value,
+        accountTo: document.querySelector(DOMstrings.destinatioAccount).value
+      };            
+      },
+    
+    // Display Account
+    // displayAccount: function(obj) {
+      
+    //   document.querySelector(DOMstrings.checkingValue).textContent = 
+    // }
+
+    getDOMstrings: function() {
+          return DOMstrings;
+    }
+  };
+})();
+
+
+var controller = (function(transfCTRL,UICtrl){
+var DOM;
 var setupEventListeners = function() {
 
   document.querySelector('.add_btn').addEventListener('click',ctrlAddAmount)
   document.addEventListener('keypress', function(event){
     if(event.keyCode ===13) {
       ctrlAddAmount();
-
     }
-  })
-}
-
+  });
+};
+  DOM = UICtrl.getDOMstrings();
+  
   var ctrlAddAmount = function() {
+    var input;
     // 1.Get the field input data.
-      var input = UICtrl.getInput();
+      input = UICtrl.getInput();
       console.log(input);
-    // console.log("App started");
-    // 2.Add the amount to checking account
+       
       }
-return  {
-  init:function() {
-    console.log("Application has started");
-    setupEventListeners();
-  }
-}
-
-})(transferController,UIContoller);
+      return {
+      init:function() {
+        console.log("Application has started");
+        setupEventListeners();
+      }
+    };
+        })(transferController,UIContoller);
 
 controller.init();
